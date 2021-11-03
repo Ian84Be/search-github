12fa201ff3e3837f835ad3ab218e52be14f28e3c
Ian Be <ian84be@gmail.com>
2021-11-03 13:46:45 -0500

    add eslint, prettier

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,41 @@
-import React, { useEffect } from "react"
-import GlobalStyles from "../styles/GlobalStyles"
+import React, { useEffect, useState } from 'react'
+import GlobalStyles from '../styles/GlobalStyles'
 import styled from 'styled-components'
 
 const SearchLabel = styled.label`
-	font-size: 2rem;
+  font-size: 2rem;
 `
 
 const SearchInput = styled.input`
-	border-radius: 6px;
-	padding: 5px 12px;
-	border: 1px solid var(--border);
-	background: var(--bg);
-	color: var(--text);
+  border-radius: 6px;
+  padding: 5px 12px;
+  border: 1px solid var(--border);
+  background: var(--bg);
+  color: var(--text);
 `
 
 function App() {
-	useEffect(()=> {
-		const githubTest = async () => {
-			const testURL = `https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc`
-			const response = await fetch(testURL).then(res=>res.json()).catch(err=>console.error(err))
-			console.log({response})
-		}
-		githubTest()
-	},[])
+  const [searchResults, setSearchResults] = useState([])
+
+  useEffect(() => {
+    const githubTest = async () => {
+      const testURL = `https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc`
+      const response = await fetch(testURL)
+        .then((res) => res.json())
+        .catch((err) => console.error(err))
+      console.log({ response })
+      setSearchResults(response)
+    }
+    githubTest()
+  }, [])
 
   return (
-    <div className="App">
-		<GlobalStyles/>
-			<SearchLabel htmlFor="searchGithub">
-			Search Github
-			</SearchLabel>
-			<SearchInput id="searchGithub" placeholder="search github" type="text"/>
+    <div>
+      <GlobalStyles />
+      <SearchLabel htmlFor="searchGithub">Search Github</SearchLabel>
+      <SearchInput id="searchGithub" placeholder="search github" type="text" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
